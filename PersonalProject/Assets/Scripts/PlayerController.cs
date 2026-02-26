570a5357f67e55ab2b74506ec8973424b28fa22e
연동준 (플밍 4기) <skg023863@gmail.com>
2026-02-26 15:36:29 +0900

    feat : 마우스 방향에 따라 Player 바라보는 방향 변경
    좌우로만 방향 변경됨

--- PersonalProject/Assets/Scripts/PlayerController.cs
+++ PersonalProject/Assets/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     private Animator _animator;
     private StateMachine _stateMachine;
     
-    public Vector2 _moveInput;
+    public Vector2 _moveInput; // Actions로부터 받을 Vector2 값 저장
     [SerializeField] private float _playerSpeed = 5f; // Player 속도
 
     // State 
@@ -33,11 +33,13 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        // 시작시 기본 State는 idle
         _stateMachine.ChangeState(idle);
     }
     void Update()
     {
         _stateMachine.Update();
+        LookMouse();
     }
 
     void FixedUpdate()
@@ -62,9 +64,28 @@ public class PlayerController : MonoBehaviour
         _moveInput = ctx.ReadValue<Vector2>();
     }
 
+    // Player의 움직임 함수
     void Movement()
     {
         _rb.linearVelocity = _moveInput * _playerSpeed;
+    }
+    
+    // Player가 마우스 방향에 따라 스프라이트 좌우 바꿔주는 함수
+    void LookMouse()
+    {
+        // 마우스 좌표값
+        Vector2 mousePoint = Camera.main.ScreenToWorldPoint(
+            Mouse.current.position.ReadValue());
+        
+        // Player의 좌표로부터 마우스 좌표까지의 방향
+        Vector2 dir = mousePoint - (Vector2)transform.position;
+        
+        // 방향벡터의 x딱 0(캐릭터 바로 위)일 경우에는 스프라이트 안 바뀜
+        if (dir.x != 0)
+        {
+            // 방향벡터의 x가 0보다 작으면 true(좌측 바라봄), 0보다 크면 false(우측 바라봄)
+            _bodySprite.flipX = dir.x < 0;
+        }
     }
     
     public void ChangeState(IState state)
@@ -72,6 +93,7 @@ public class PlayerController : MonoBehaviour
         _stateMachine.ChangeState(state);
     }
     
+    // value 값이 0.1 초과시 Move, 0.1 미만이면 Idle 애니메이션 재생
     public void SetMove(float value)
     {
         _animator.SetFloat("Move", Mathf.Abs(value));
