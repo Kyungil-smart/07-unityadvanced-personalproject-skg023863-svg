f02439ad14a7a6a2d739c5fbbb6e7c54b00ef2dd
연동준 (플밍 4기) <skg023863@gmail.com>
2026-02-26 18:05:06 +0900

    feat : 무기 구현
    캐릭터 주변을 따라 원을 그리듯이 무기 움직임

--- PersonalProject/Assets/Scripts/MoveState.cs
+++ PersonalProject/Assets/Scripts/MoveState.cs
@@ -11,12 +11,14 @@ public class MoveState : IState
     }
     public void Enter()
     {
+        // Move 애니메이션 실행
         _player.SetMove(0.2f);
     }
 
     public void Update()
     {
-        if (_player._moveInput == Vector2.zero)
+        // MoveInput이 (0, 0)이면 Idle 실행
+        if (_player.moveInput == Vector2.zero)
         {
             _player.ChangeState(_player.idle);
         }
